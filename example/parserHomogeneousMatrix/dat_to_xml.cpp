40284c4fede48f38849cb56154b7273f451a8598
jokla <giovanni.claudio@inria.fr>
2014-11-12 16:25:43 +0100

    Added conversion example from .dat to xml file

---
+++ example/parserHomogeneousMatrix/dat_to_xml.cpp
@@ -0,0 +1,85 @@
+/****************************************************************************
+ *
+ * This file is part of the ViSP software.
+ * Copyright (C) 2005 - 2014 by INRIA. All rights reserved.
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * ("GPL") version 2 as published by the Free Software Foundation.
+ * See the file LICENSE.txt at the root directory of this source
+ * distribution for additional information about the GNU GPL.
+ *
+ * For using ViSP with software that can not be combined with the GNU
+ * GPL, please contact INRIA about acquiring a ViSP Professional
+ * Edition License.
+ *
+ * See http://team.inria.fr/lagadic/visp for more information.
+ *
+ * This software was developed at:
+ * INRIA Rennes - Bretagne Atlantique
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * France
+ * http://team.inria.fr/lagadic
+ *
+ * If you have questions regarding the use of this file, please contact
+ * INRIA at visp@inria.fr
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Description:
+ * This example demonstrates how to control the robot remotely in position and velocity.
+ *
+ * Authors:
+ * Giovanni Claudio
+ *
+ *****************************************************************************/
+
+/*! \example dat_to_xml.cpp */
+#include <iostream>
+#include <string>
+
+#include<visp_naoqi/vpXmlParserHomogeneousMatrix.h>
+#include<visp_naoqi/vpNaoqiConfig.h>
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    //Create Pose Vector and convert to Homogeneous Matrix
+    vpHomogeneousMatrix eMc;
+
+    std::ifstream f("eMc.dat") ;
+    eMc.load(f) ;
+    f.close() ;
+    std::cout << "eMc:\n" << eMc << std::endl;
+
+#ifdef VISP_HAVE_XML2
+    vpXmlParserHomogeneousMatrix p; // Create a XML parser
+    std::string name_M =  "eMc_CameraLeft_with_distorsion";
+    char filename[FILENAME_MAX];
+    sprintf(filename, "%s", VISP_NAOQI_EXTRINSIC_CAMERA_FILE);
+
+    if (p.save(eMc, filename, name_M) != vpXmlParserHomogeneousMatrix::SEQUENCE_OK)
+    {
+      std::cout << "Cannot save the Homogeneous matrix" << std::endl;
+    }
+
+
+  }
+  catch (const vpException &e)
+  {
+    std::cerr << "Caught exception: " << e.what() << std::endl;
+  }
+
+  vpXmlParser::cleanup();
+
+#endif
+  return 0;
+}
+
+
+
+
+
